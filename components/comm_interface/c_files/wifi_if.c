604f969ca3a09e1addb674e27c2a1697a5913f4d
lucianovr <rluciano025@gmail.com>
2021-04-25 23:21:31 -0300

    Add WiFi interface

---
+++ components/comm_interface/c_files/wifi_if.c
@@ -0,0 +1,161 @@
+#include "comm_if.h"
+
+#include <string.h>
+#include "sdkconfig.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_wifi_default.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+// PRIVATE CONSTANTS
+#define TAG "wifi_if"
+
+// PRIVATE DATA TYPES
+
+// PRIVATE VARIABLES
+static esp_netif_t *s_example_esp_netif = NULL;
+static esp_netif_t *esp_netif = NULL;
+static esp_ip4_addr_t s_ip_addr;
+
+static comm_if_connected_cb_t connected_cb = NULL;
+static comm_if_disconnected_cb_t disconnected_cb = NULL;
+
+// PRIVATE FUNCTIONS
+static esp_netif_t* wifi_start(void);
+static void wifi_stop(void);
+static void on_wifi_connect(void *esp_netif, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void on_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+
+// PUBLIC FUNCTIONS IMPLEMENTATIONS
+
+void comm_if_init(void)
+{
+
+}
+
+void comm_if_deinit(void)
+{
+
+}
+
+void comm_if_start(void)
+{
+    s_example_esp_netif = wifi_start();
+
+    ESP_ERROR_CHECK(esp_register_shutdown_handler(&wifi_stop));
+}
+
+void comm_if_stop(void)
+{
+    wifi_stop();
+}
+
+
+void comm_if_reg_connected_cb(comm_if_connected_cb_t cb)
+{
+    connected_cb = cb;
+}
+
+void comm_if_reg_disconnected_cb(comm_if_disconnected_cb_t cb)
+{
+    disconnected_cb = cb;
+}
+
+
+// PRIVATE FUNCTION IMPLEMENTATION
+
+static esp_netif_t* wifi_start(void)
+{
+    char *desc;
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+    // Prefix the interface description with the module TAG
+    // Warning: the interface desc is used in tests to capture actual connection details (IP, gw, mask)
+    asprintf(&desc, "%s: %s", TAG, esp_netif_config.if_desc);
+    esp_netif_config.if_desc = desc;
+    esp_netif_config.route_prio = 128;
+    esp_netif_t *netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+    free(desc);
+    esp_wifi_set_default_wifi_sta_handlers();
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect, netif));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
+
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_EXAMPLE_WIFI_SSID,
+            .password = CONFIG_EXAMPLE_WIFI_PASSWORD,
+        },
+    };
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    return netif;
+}
+
+static void wifi_stop(void)
+{
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
+    
+    esp_err_t err = esp_wifi_stop();
+    if (err == ESP_ERR_WIFI_NOT_INIT) {
+        return;
+    }
+    ESP_ERROR_CHECK(err);
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+    ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(s_example_esp_netif));
+    esp_netif_destroy(s_example_esp_netif);
+    s_example_esp_netif = NULL;
+}
+
+
+
+static void on_wifi_connect(void *esp_netif, esp_event_base_t event_base,
+                            int32_t event_id, void *event_data)
+{
+}
+
+static void on_got_ip(void *arg, esp_event_base_t event_base,
+                      int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
+    memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+
+
+    if( connected_cb )
+    {
+        connected_cb();
+    }
+}
+
+
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if( disconnected_cb )
+        disconnected_cb();
+        
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    esp_err_t err = esp_wifi_connect();
+    if (err == ESP_ERR_WIFI_NOT_STARTED) {
+        return;
+    }
+    ESP_ERROR_CHECK(err);
+}
